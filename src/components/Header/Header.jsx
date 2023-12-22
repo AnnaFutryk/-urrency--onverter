9122b2d058b3cbefcb7bdff0729cc626d682242c
AnnaFutryk <anna.futryk@gmail.com>
2023-12-22 04:33:29 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,36 @@
+import React, { useEffect, useState } from "react";
 import { HeaderWrap, List } from "./Header.styled";
 
-const Header = () => {
+const Header = ({ baseCurrency }) => {
+  const API_KEY = "dd3767f7553d65a03398bebbe4d7e2c9";
+  const [exchangeRates, setExchangeRates] = useState({ USD: 0, EUR: 0 });
+
+  useEffect(() => {
+    fetch(
+      `http://api.exchangeratesapi.io/latest?access_key=${API_KEY}&base=${baseCurrency}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.rates) {
+          setExchangeRates({
+            USD: data.rates.USD || 0,
+            EUR: data.rates.EUR || 0,
+          });
+        } else {
+          console.error(
+            "Invalid response format or missing exchange rates:",
+            data
+          );
+        }
+      })
+      .catch((error) => console.error("Error fetching exchange rates:", error));
+  }, [baseCurrency]);
+
   return (
     <HeaderWrap>
       <List>
-        <li>USD</li>
-        <li>EUR</li>
+        <li>USD: {exchangeRates.USD}</li>
+        <li>EUR: {exchangeRates.EUR}</li>
       </List>
     </HeaderWrap>
   );
