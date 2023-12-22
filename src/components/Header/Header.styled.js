92684784edcf47821752b9ba4e85e62c40a57eb9
AnnaFutryk <anna.futryk@gmail.com>
2023-12-22 03:44:30 +0200

    Create Header.styled.js

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const HeaderWrap = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 56px;
+  display: flex;
+  align-items: center;
+
+  padding-top: 6px;
+  padding-bottom: 6px;
+  background-color: rgba(0, 0, 0, 0.6);
+  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.5);
+  z-index: 10;
+`;
+
+export const List = styled.ul`
+  list-style-type: none;
+  display: flex;
+  gap: 30px;
+`;
