c39134dfde8ab26a848d62ebd281e5e5018c310d
AnnaFutryk <anna.futryk@gmail.com>
2023-12-22 12:41:52 +0200

    change api

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Converter from "./components/Converter/Converter";
 import Header from "./components/Header/Header";
 import { Equal, SpanFirst, SpanSecond } from "./App.styled";
 
-const API_KEY = "dd3767f7553d65a03398bebbe4d7e2c9";
-const BASE_URL = `http://api.exchangeratesapi.io/latest?access_key=${API_KEY}`;
+// const API_KEY = "dd3767f7553d65a03398bebbe4d7e2c9";
+// const BASE_URL = `http://api.exchangeratesapi.io/latest?access_key=${API_KEY}`;
+const BASE_URL = `https://api.frankfurter.app/latest`;
 
 function App() {
   const [currencyOpts, setCurrencyOpts] = useState([]);
@@ -29,6 +30,7 @@ function App() {
     fetch(BASE_URL)
       .then((res) => res.json())
       .then((data) => {
+        console.log(data);
         const firstCurrency = Object.keys(data.rates)[0];
         setCurrencyOpts([data.base, ...Object.keys(data.rates)]);
         setFromCurrency(data.base);
@@ -40,7 +42,7 @@ function App() {
 
   useEffect(() => {
     if (fromCurrency && toCurrency) {
-      fetch(`${BASE_URL}&base=${fromCurrency}&symbols=${toCurrency}`)
+      fetch(`${BASE_URL}?from=${fromCurrency}&to=${toCurrency}`)
         .then((res) => res.json())
         .then((data) => {
           if (data.rates && data.rates[toCurrency] !== undefined) {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from "react";
 import { HeaderWrap, List } from "./Header.styled";
 
 const Header = ({ baseCurrency }) => {
-  const API_KEY = "dd3767f7553d65a03398bebbe4d7e2c9";
+  // const API_KEY = "dd3767f7553d65a03398bebbe4d7e2c9";
   const [exchangeRates, setExchangeRates] = useState({ USD: 0, EUR: 0 });
 
   useEffect(() => {
     fetch(
-      `http://api.exchangeratesapi.io/latest?access_key=${API_KEY}&base=EUR`
+      // `http://api.exchangeratesapi.io/latest?access_key=${API_KEY}&base=EUR`
+      "https://api.frankfurter.app/latest"
     )
       .then((res) => res.json())
       .then((data) => {
         if (data.rates) {
           setExchangeRates({
-            USD: (data.rates.USD * data.rates.UAH || 0).toFixed(2),
-            EUR: (data.rates.EUR * data.rates.UAH || 0).toFixed(2),
+            USD: (data.rates.USD * data.rates.PLN || 0).toFixed(2),
+            EUR: (data.base * data.rates.PLN || 0).toFixed(2),
+            // USD: (data.rates.USD * data.rates.UAH || 0).toFixed(2),
+            // EUR: (data.rates.EUR * data.rates.UAH || 0).toFixed(2),
           });
         } else {
           console.error(
