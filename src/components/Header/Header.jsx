11b572bc7fa3c4b9217591726e922bee96bcc7f1
AnnaFutryk <anna.futryk@gmail.com>
2023-12-22 03:44:27 +0200

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { HeaderWrap, List } from "./Header.styled";
+
+const Header = () => {
+  return (
+    <HeaderWrap>
+      <List>
+        <li>USD</li>
+        <li>EUR</li>
+      </List>
+    </HeaderWrap>
+  );
+};
+
+export default Header;
